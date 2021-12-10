4ce519bd10563e675b53ce8491829648acfb0b4b
Michael <mishat@gmail.com>
2021-12-10 11:26:23 +0200

    Working

--- models/dcgan/discriminator.py
+++ models/dcgan/discriminator.py
@@ -5,10 +5,12 @@ class Discriminator(nn.Module):
     def __init__(self, images_channels: int, images_width: int, images_height: int):
         super(Discriminator, self).__init__()
 
-        def discriminator_block(in_filters, out_filters, bn=True):
-            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
+        def discriminator_block(in_channels: int, out_channels: int, bn: bool = True):
+            block = [nn.Conv2d(in_channels, out_channels, 3, 2, 1),
+                     nn.LeakyReLU(0.2, inplace=True),
+                     nn.Dropout2d(0.25)]
             if bn:
-                block.append(nn.BatchNorm2d(out_filters, 0.8))
+                block.append(nn.BatchNorm2d(out_channels, 0.8))
             return block
 
         self.model = nn.Sequential(*discriminator_block(images_channels, 16, bn=False),
@@ -18,7 +20,8 @@ class Discriminator(nn.Module):
 
         # The height and width of downsampled image
         ds_size = images_width // 2 ** 4
-        self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
+        self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1),
+                                       nn.Sigmoid())
 
     def forward(self, image):
         out = self.model(image)
